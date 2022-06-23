dc893077b68e84aa8097c612e7287e9786f95f44
valterhre <50119287+valterhre@users.noreply.github.com>
2022-06-23 11:19:48 +0300

    Add files via upload

--- lis.py
+++ lis.py
@@ -3,7 +3,7 @@ import requests
 
 for num in range(1,9):
     print(num)
-    for serie in range(1,21):
+    for serie in range(1,25):
         serie = str(serie).zfill(2)
         stuff=f's0{num}'
         url = f'http://vihostdgs40.com/video/8ba1d9cac337890b21e1cffd891b67a9/serials/Charmed/{stuff}/Charmed.S0{num}E{serie}.mp4'
@@ -16,11 +16,14 @@ for num in range(1,9):
         if name in  os.listdir():
             continue
         video=requests.get(url)
+        if video.status_code!=200:
+            print('something wrong happend')
+            continue
         k=0
         chunk_size = 50000
         for chunk in video.iter_content(chunk_size=chunk_size):
             k=k+1
             print((chunk_size)/(float(video.headers['Content-Length']))*k*100)
         print(name)
-        with open(f'./charmentStuff{num}/{name}', 'wb') as w:
+        with open(f'D:/pythonProject21/some/charmentStuff{num}/{name}', 'wb') as w:
             w.write(video.content)
